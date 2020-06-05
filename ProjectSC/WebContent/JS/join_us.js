10524e524d02c36a6f4b059dac7ca23a24eba19b
sara <kimbogyeum1@naver.com>
2020-06-05 17:58:10 +0900

    6/5 커밋

--- ProjectSC/WebContent/JS/join_us.js
+++ ProjectSC/WebContent/JS/join_us.js
@@ -22,6 +22,18 @@ signUpButton.addEventListener('click', function() {
 // 유효성 검사---------------------------------------------------------------------------------
   $("#formstore").validate({ // joinForm에 validate를 적용
     rules: {
+    	  
+        StoreNumber: {
+            required: true,
+            number: true,
+            rangelength: [10, 15]
+        },
+        
+        StoreName: {
+            required: true
+        },
+
+    	
         Id: {
             required: true,
             rangelength: [5, 10]
@@ -47,37 +59,38 @@ signUpButton.addEventListener('click', function() {
             email: true
         },
         
-        StoreName: {
-            required: true
-        },
-        StoreNumber: {
-            required: true,
-            number: true,
-            rangelength: [10, 15]
-        },
        
 
     },
     messages: {
+    	
+    	StoreNumber: {
+    		required: "매장번호는 필수 입니다",
+    		number:"숫자만 입력해주세요",
+    		rangelength: "매장번호는 10자리 이상입니다."
+    	},
+    	
+    	StoreName:{
+    		required: "매장 이름을 다시 입력해주세요"
+    	},
+    	
         Id: "아이디를 다시 입력해주세요",
+ 
         PhoneNumber: {
             required: "전화번호는 필수 입니다",
             number:"숫자만 입력하세요",
             rangelength: "전화번호는 10자리 이상입니다."
         },
+        
         Password: {
             required: "비밀번호는 필수 입니다",
             rangelength: "비밀번호는 8자리 이상입니다."
         },
+        
         ConfirmPw: "똑같은 비밀번호를 입력해주세요",
         Email: "유효한 이메일을 입력해주세요"
     	},
     	
-    	StoreNumber: {
-            required: "매장번호는 필수 입니다",
-            number:"숫자만 입력해주세요",
-            rangelength: "매장번호는 10자리 이상입니다."
-        },
 
 });
 
