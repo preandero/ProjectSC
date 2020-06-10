f2740207e037500c14ba650d124e280b304d7392
108231ju <108231ju@naver.com>
2020-06-10 11:08:26 +0900

    o

--- ProjectSC/src/command/CSListCommand.java
+++ ProjectSC/src/command/CSListCommand.java
@@ -13,33 +13,33 @@ import pos.beans.MemberDTO;
 
 public class CSListCommand implements Command {
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession(false);
-		CS_WriteDAO dao = new CS_WriteDAO();
-		CS_WriteDTO[] arr = null;  //초기값
-		int mem_uid = (int) session.getAttribute("mem_uid");
-		// TODO 세션이 잘 넘어오는지 확인해줘
-		
-		try {
-			//트랜잭션수행
-			arr = dao.selectCSbyId(mem_uid);
-			
-			System.out.println(arr.toString());
-			
-			//결과를 담아야한다.  결과 = arr ,  바구니 = request
-			// list 란 name 으로 request에 arr 값 전달
-			//즉 request에 담아서 컨트롤러에 전달되는 셈.
-			request.setAttribute("list", arr);
-			// list 로 set 했으니 나중에 꺼낼때도 (get 할때도 ) list로 꺼내야함
-			//잊지마세염
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-			
-			//만약에 Connection Pool을 사용한다면
-			//Naming Exception 도 처리해줘야함
-		}
-	}
+   @Override
+   public void execute(HttpServletRequest request, HttpServletResponse response) {
+      HttpSession session = request.getSession(false);
+      CS_WriteDAO dao = new CS_WriteDAO();
+      CS_WriteDTO[] arr = null;  //초기값
+      int mem_uid = (int) session.getAttribute("mem_uid");
+      // TODO 세션이 잘 넘어오는지 확인해줘
+      
+      try {
+         //트랜잭션수행
+         arr = dao.selectCSbyId(mem_uid);
+         
+         System.out.println(arr.toString());
+         
+         //결과를 담아야한다.  결과 = arr ,  바구니 = request
+         // list 란 name 으로 request에 arr 값 전달
+         //즉 request에 담아서 컨트롤러에 전달되는 셈.
+         request.setAttribute("list", arr);
+         // list 로 set 했으니 나중에 꺼낼때도 (get 할때도 ) list로 꺼내야함
+         //잊지마세염
+         
+      }catch(SQLException e) {
+         e.printStackTrace();
+         
+         //만약에 Connection Pool을 사용한다면
+         //Naming Exception 도 처리해줘야함
+      }
+   }
 
 }
--- ProjectSC/src/cs_board/beans/CS_WriteDTO.java
+++ ProjectSC/src/cs_board/beans/CS_WriteDTO.java
@@ -7,7 +7,7 @@ public class CS_WriteDTO {
 	private String content;		
 	private String regDate;
 	private int mem_uid;
-	private String m_id;
+	private String mem_id;
 	
 
 
@@ -26,13 +26,12 @@ public class CS_WriteDTO {
 		this.mem_uid = m_uid;
 	}
 	
-	public CS_WriteDTO(int uid, String subject, String content,int m_uid, String m_id) {
+	public CS_WriteDTO(int uid, String subject, String content, String mem_id) {
 		super();
 		this.uid = uid;
 		this.subject = subject;
 		this.content = content;
-		this.mem_uid = m_uid;
-		this.m_id = m_id;
+		this.mem_id = mem_id;
 	}
 	
 	
